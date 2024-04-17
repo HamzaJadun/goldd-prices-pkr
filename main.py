f7ff6bea1fdd683248272bf1f944b92a515f90ca
Hamza Jadun <81479337+HamzaJadun@users.noreply.github.com>
2024-04-18 00:11:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -32,11 +32,11 @@ def get_gold_rates():
     
     return data
 
+@app.get('/')
+async def index():
+    return 'Welcome to the Gold Rates API'
+
 @app.get('/gold-rates')
 async def gold_rates_api():
     gold_rates_data = get_gold_rates()
     return gold_rates_data
-
-@app.get('/')
-async def index():
-    return 'Welcome to the Gold Rates API'
