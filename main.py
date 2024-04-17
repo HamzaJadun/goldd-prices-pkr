dc356346234758c47a38c5867121362b18046ea7
Hamza Jadun <81479337+HamzaJadun@users.noreply.github.com>
2024-04-18 00:03:26 +0500

    Update and rename app.py to main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from flask import Flask, jsonify
+from fastapi import FastAPI, HTTPException, Response
 from bs4 import BeautifulSoup
 import requests
 
-app = Flask(__name__)
+app = FastAPI()
 
 def get_gold_rates():
     url = 'https://www.urdupoint.com/business/gold-rates.html'
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-
+    
     # Find the table containing gold rates
     table = soup.find('table', class_='resp_table mb0')
-
+    
     # Extract data from each row of the table
     rows = table.find_all('tr')
     data = []
@@ -21,7 +21,7 @@ def get_gold_rates():
             purity = cells[0].text.strip()
             per_tola = cells[1].text.strip()
             per_10_gram = cells[2].text.strip()
-
+            
             # Creating a dictionary for each row of data
             row_data = {
                 'purity': purity,
@@ -29,18 +29,14 @@ def get_gold_rates():
                 'per_10_gram': per_10_gram,
             }
             data.append(row_data)
-
+    
     return data
 
-@app.route('/gold-rates', methods=['GET'])
-def gold_rates_api():
+@app.get('/gold-rates')
+async def gold_rates_api():
     gold_rates_data = get_gold_rates()
-    return jsonify(gold_rates_data)
+    return gold_rates_data
 
-# Define a route for the root URL
-@app.route('/')
-def index():
+@app.get('/')
+async def index():
     return 'Welcome to the Gold Rates API'
-
-if __name__ == '__main__':
-    app.run(debug=True)
